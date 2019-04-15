228d3f61c10663b3d2f99a8676ae1abe5a854a22
yagormess <49286161+yagormess@users.noreply.github.com>
2019-04-15 10:40:52 -0300

    Add files via upload

--- script.R
+++ script.R
@@ -3,21 +3,25 @@ library(descriptr)
 library(dplyr)
 library(gsheet)
 library(markdown)
+library(ggplot2)
 #########
 
 #######Carregar a tabela na variável "a" e limpar os dados########
 a<-gsheet2tbl("https://docs.google.com/open?id=1qegBjG-GkGQHwhnkzOmLNbI_pddNMs7el-bwlPlDEnA")
 c<-filter(a, Item_Weight != "null" & Item_Identifier != "null" & Outlet_Size != "")
-rm(a)
+rm(d)
 #########
 
 
 summary(c$Item_Fat_Content)
-c$Item_Fat_Content[c$Item_Fat_Content=="LF"]<-"Low Fat"
-c$Item_Fat_Content[c$Item_Fat_Content=="low fat"]<-"Low Fat"
-d<- c %>% count(Item_Type, Outlet_Location_Type)
-c$Item_WeightCat<-cut(c$Item_Weight,seq(0,50,5))
+
+c$FaixadePeso<-cut(c$Item_Weight,seq(0,25,5))
 c$Item_Outlet_Sales2<-cut(c$Item_Outlet_Sales,seq(0,11000,1000))
 e<-ds_twoway_table(c, Item_WeightCat, Item_Outlet_Sales2)
-ggplot(e, aes(Item_WeightCat,percent))+
-  geom_col()
+correla<-c(rcorr(cbind(c$Item_Weight, c$Item_MRP, c$Item_Outlet_Sales)))
+ggplot(c, aes(x=d$Item_MRP, y=d$Item_Outlet_Sales))+
+  geom_point()
+
+teste<-ggplot(d,aes(Item_MRP,Item_Outlet_Sales))+
+  geom_point()+
+  labs(x="Preço de Fabrica",y="Quantidade de itens Vendidos")+geom_jitter(aes(colour=c$Item_Fat_Content))
